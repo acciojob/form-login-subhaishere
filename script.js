690f36dd444d0f421b75a4a46c93644857ccc26e
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-25 00:23:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+   let formName = document.getElementById("form1");
+	let FirstName = document.getElementById("first").value;
+	let LastName = document.getElementById("last").value;
+	alert(FirstName + " "+ LastName);
 }
